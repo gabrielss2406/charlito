21a6ab7358091e4ea60cf843c9bef7ca97604a5b
gabrielss2406 <59343269+gabrielss2406@users.noreply.github.com>
2020-11-27 14:18:47 -0300

    Add files via upload

---
+++ script/jquery.js
@@ -0,0 +1,38 @@
+var cont = 0
+
+function mini(){
+    cont++
+
+    if( (cont%2) == 0 ){
+        $("#chatbot").animate({
+            bottom:"-54vh"
+        })
+        $('#img').animate({
+            opacity:"1"
+        })
+        $('btn').animate({
+            opacity:'0'
+        })
+    }
+
+    else{
+        $("#chatbot").animate({
+            bottom:"5vh"
+        })
+        $('#img').animate({
+            opacity:"0"
+        })
+        $('btn').animate({
+            opacity:'1'
+        })
+    }
+}
+
+function refreshIframe() {
+    var ifr = document.getElementById('iframe');
+    ifr.src = ifr.src;
+}
+
+function clickBackground(){
+    window.location.href = "http://www.zenitglass.com.br"
+}
